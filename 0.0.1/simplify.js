c46ae30969e30a0b27c46cb0bf810c4ce87d33c3
Vinicius Sueiro <hi@vsueiro.com>
2018-09-05 21:16:45 -0300

    Standardizes the use of semicolons and indentation

--- 0.0.1/simplify.js
+++ 0.0.1/simplify.js
@@ -1,57 +1,60 @@
 function the( thing, parent ) {
-  var item, list;
 
-  parent = parent || document;
+  let item, list
 
-  item = document.getElementById( thing );
+  parent = parent || document
+
+  item = document.getElementById( thing )
   if ( item !== null ) return item
 
-  list = parent.getElementsByClassName( thing );
+  list = parent.getElementsByClassName( thing )
   if ( list.length ) return list[ 0 ]
 
-  list = parent.getElementsByTagName( thing );
+  list = parent.getElementsByTagName( thing )
   if ( list.length ) return list[ 0 ]
 
-  item = parent.querySelector( thing );
+  item = parent.querySelector( thing )
   if ( item !== null ) return item
 
-	return null
+  return null
 }
 
 function them( thing, parent ) {
-  var list;
+
+  let list
   
-  parent = parent || document;
+  parent = parent || document
 
-  list = parent.getElementsByClassName( thing );
+  list = parent.getElementsByClassName( thing )
   if ( list.length ) return list
 
-  list = parent.getElementsByTagName( thing );
+  list = parent.getElementsByTagName( thing )
   if ( list.length ) return list
 
-  list = parent.querySelectorAll( thing );
+  list = parent.querySelectorAll( thing )
   if ( list.length ) return list
 
-	return null
+  return null
+
 }
 
 function each( thing, callback, parent ) {
 
-  var list, isArray, get, total;
+  let list, isArray, get, total
 
-  parent = parent || document;
+  parent = parent || document
   
   get = function( thing ) {
     
     if ( typeof thing === 'string' ) {
 
-      list = parent.getElementsByClassName( thing );
+      list = parent.getElementsByClassName( thing )
       if ( list.length ) return list
 
-      list = parent.getElementsByTagName( thing );
+      list = parent.getElementsByTagName( thing )
       if ( list.length ) return list
 
-      list = parent.querySelectorAll( thing );
+      list = parent.querySelectorAll( thing )
       if ( list.length ) return list
 
       return false
@@ -60,20 +63,20 @@ function each( thing, callback, parent ) {
     return thing
   }
 
-  list = get( thing );
+  list = get( thing )
 
   if ( !list ) return false
 
-	isArray = list.constructor === Array ? true : false;
+  isArray = list.constructor === Array ? true : false
 
-  total = typeof list === 'number' ? list : list.length;
+  total = typeof list === 'number' ? list : list.length
   
-	for ( var i = 0 ; i < total ; i++ ) {
+  for ( var i = 0 ; i < total ; i++ ) {
 
-		if ( isArray ) callback.call( list[ i ], list[ i ], i )
-		else           callback.call( list[ i ], i )
+    if ( isArray ) callback.call( list[ i ], list[ i ], i )
+    else           callback.call( list[ i ], i )
 
-	}
+  }
 
   return null
 }
